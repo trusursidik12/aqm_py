40e5f569009cdb3df314cc54485e9f5963326998
warihsuryono <warih_suryono@yahoo.com>
2020-02-22 08:10:08 +0700

    aaa

--- sensor_reader.py
+++ sensor_reader.py
@@ -150,10 +150,12 @@ for port in serial_ports():
     p = subprocess.Popen('dmesg | grep ' + str(port).replace('/dev/','') + ' | tail -1', stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
-    try:
-        port_desc = output.decode("utf-8").split(" now attached")[0].split(":")[1]
-    except:
-        port_desc = output.decode("utf-8")
+    port_desc = output.decode("utf-8")
+    if "now attached" in port_desc:
+        try:
+            port_desc = port_desc.split(":")[1].split(" now attached")[0]
+        except:
+            port_desc = port_desc
     print(port_desc)
     mycursor.execute("INSERT INTO serial_ports (port,description) VALUES ('" + port +"','" + port_desc +"')")
     mydb.commit()
