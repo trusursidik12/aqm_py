f1020b26160e4732b2d6e5cc60cb3187c9faaf72
warihsuryono <warih_suryono@yahoo.com>
2020-02-24 09:22:11 +0700

    aaa

--- aqm_show.sh
+++ aqm_show.sh
@@ -2,7 +2,7 @@
 echo "Reading devices... Please wait!!"
 sleep 20s
 ls /dev/ttyUSB*
-echo "wait for internet connection...!!"
+echo "wait for localhost...!!"
 sleep 20s
 ping 127.0.0.1 -c 20
 echo admin | sudo -S python3.5 ~/aqm_py/sensor_reader.py &
