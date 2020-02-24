027ad1a08359a9d554401eddb64fec69040ca444
warihsuryono <warih_suryono@yahoo.com>
2020-02-24 08:53:20 +0700

    delaying

--- aqm_show.sh
+++ aqm_show.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Reading devices... Please wait!!"
 sleep 20s
 ls /dev/ttyUSB*
 sleep 20s
 echo admin | sudo -S python3.5 ~/aqm_py/sensor_reader.py &
 sleep 20s
+echo "Please wait!!"
 python3.5 ~/aqm_py/aqm_show.py
