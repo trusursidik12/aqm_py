8c82cb29ce444659c781e58f7dc6a87dc301285a
warihsuryono <warih_suryono@yahoo.com>
2020-03-01 19:53:53 +0700

    aaa

--- aqm_show.py
+++ aqm_show.py
@@ -3,16 +3,34 @@
 # import webview
 # webview.create_window('AQM', 'http://127.0.0.1/aqmmaster',frameless=True,fullscreen=True)
 # webview.start()
-import sys
-from PyQt5.Qt import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtWidgets import QApplication
 
-app = QApplication(sys.argv)
-web = QWebEngineView()
-web.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
-web.settings().setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, True)
-web.settings().setAttribute(QWebEngineSettings.LocalStorageEnabled, True)
-web.load(QUrl("http://127.0.0.1/aqmmaster"))
-web.showMaximized()
-sys.exit(app.exec_())
+from PyQt5 import QtWebEngineWidgets, QtWidgets, QtCore
+
+class WebEnginePage(QtWebEngineWidgets.QWebEnginePage):
+    def __init__(self, *args, **kwargs):
+        QtWebEngineWidgets.QWebEnginePage.__init__(self, *args, **kwargs)
+        self.profile().downloadRequested.connect(self.on_downloadRequested)
+
+    @QtCore.pyqtSlot(QtWebEngineWidgets.QWebEngineDownloadItem)
+    def on_downloadRequested(self, download):
+        old_path = download.path()
+        suffix = QtCore.QFileInfo(old_path).suffix()
+        path, _ = QtWidgets.QFileDialog.getSaveFileName(self.view(), "Save File", old_path, "*."+suffix)
+        if path:
+            download.setPath(path)
+            download.accept()
+            
+if __name__ == '__main__':
+    import sys
+
+    sys.argv.append("--remote-debugging-port=8000")
+    sys.argv.append("--disable-web-security")
+
+    app = QtWidgets.QApplication(sys.argv)
+    view = QtWebEngineWidgets.QWebEngineView()
+    page = WebEnginePage(view)
+    view.setPage(page)
+    view.load(QtCore.QUrl("http://127.0.0.1/aqmmaster"))
+    view.show()
+    sys.exit(app.exec_())
+
