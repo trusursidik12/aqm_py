604ec5b3cdb043daeec3e10b986e22fcfbe0da6b
warihsuryono <warih_suryono@yahoo.com>
2020-03-01 19:14:01 +0700

    aaa

--- aqm_show.py
+++ aqm_show.py
@@ -10,6 +10,10 @@ from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 web = QWebEngineView()
+web.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+web.settings().setAttribute(QWebEngineSettings.JavascriptEnabl‌​ed, True) 
+web.settings().setAttribute(QWebEngineSettings.FullScreenSuppo‌​rtEnabled, True)
+web.page().fullScreenRequested.connect(QWebEngineFullScreenReq‌​uest.accept) 
 web.load(QUrl("http://127.0.0.1/aqmmaster/"))
-web.show()
+web.showMaximized() 
 sys.exit(app.exec_())
