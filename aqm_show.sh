2605b3dc2522507b29dba57f347fd86bb9214b5e
warih suryono <warih_suryono@yahoo.com>
2020-09-08 08:02:28 +0700

    aaa

--- aqm_show.sh
+++ aqm_show.sh
@@ -9,8 +9,8 @@ echo admin | sudo -S echo 0403 cc60 >/sys/bus/usb-serial/drivers/ftdi_sio/new_id
 sleep 1s
 ls /dev/ttyUSB*
 echo "wait for localhost...!!"
-sleep 5s
+sleep 1s
 ping 127.0.0.1 -c 5
 echo admin | sudo -S python3.5 ~/aqm_py/sensor_reader.py &
-sleep 10s
+sleep 5s
 python3.5 ~/aqm_py/aqm_show.py
