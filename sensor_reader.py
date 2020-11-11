33653b7c8ab96859a9cbb2c52e3e84f686afa167
warih suryono <warih_suryono@yahoo.com>
2020-11-12 06:50:06 +0700

    aaa

--- sensor_reader.py
+++ sensor_reader.py
@@ -396,7 +396,7 @@ while True:
         try :
             mycursor.execute("SELECT WS FROM aqm_sensor_values WHERE id = '1'")
             rec = mycursor.fetchall()
-            print(res)
+            print(rec)
             WS = rec[0]
         except Exception as e:
             print(e)
