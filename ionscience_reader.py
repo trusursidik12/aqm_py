96f12e046f65103c97c7bf3e79a2d1740dfc8566
warihsuryono <warih_suryono@yahoo.com>
2020-12-28 11:34:01 +0700

    aaa

--- ionscience_reader.py
+++ ionscience_reader.py
@@ -6,28 +6,63 @@ import serial
 from mysql.connector.constants import ClientFlag
 import mysql.connector
 import time
-import serial.rs485
 
-serial_port='COM5'
-serial_rate=19200
+is_IONSCIENCE_connect = False
 
-ion=minimalmodbus.Instrument(serial_port,1)
-ion.serial.baudrate=serial_rate
-ion.serial.parity=serial.PARITY_NONE
-ion.serial.bytesize=8
-ion.serial.stopbits=1
-ion.mode=minimalmodbus.MODE_RTU
-ion.serial.timeout=0.05
-modbus=ion.write_register(0x1248,1)
+try:
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="root", database="trusur_aqm")
+    mycursor = mydb.cursor()
+
+    print("[V] IONSCIENCE Database CONNECTED")
+except Exception as e:
+    print("  [X] IONSCIENCE " + e)
 
 
-#ser = serial.rs485.RS485(port=serial_port, baudrate=serial_rate)
-#ser.rs485_mode = serial.rs485.RS485Settings(rts_level_for_tx=False, rts_level_for_rx=True, delay_before_tx=0.0, delay_before_rx=0.0)
-#client = ModbusSerialClient(method='rtu')
-#client.socket = ser
-#client.connect()
-#client.write_register(0x1248,0x01);
-# time.sleep(3)
-#result = client.read_holding_registers(0x1202, count=2, unit=1)
-#print(result.registers)
-#client.close()
+def connect_ionscience(ionsciencemode):
+    global is_IONSCIENCE_connect
+    try:
+        mycursor.execute("SELECT content FROM aqm_configuration WHERE data = 'com_ion_science'")
+        rec = mycursor.fetchone()
+        for row in rec:
+            serial_port = rec[0].split(";")[ionsciencemode]
+
+        mycursor.execute("SELECT content FROM aqm_configuration WHERE data = 'baud_ion_science'")
+        rec = mycursor.fetchone()
+        for row in rec:
+            serial_rate = rec[0].split(";")[ionsciencemode]
+            
+        
+        ion=minimalmodbus.Instrument(serial_port,1)
+        ion.serial.baudrate=serial_rate
+        ion.serial.parity=serial.PARITY_NONE
+        ion.serial.bytesize=8
+        ion.serial.stopbits=1
+        ion.mode=minimalmodbus.MODE_RTU
+        ion.serial.timeout=0.05
+        ion.write_register(0x1248,1)
+        
+        response=ion.read_registers(0x1260,12,4)
+        return response
+        # response=ion.read_registers(0x12c0,12,4)
+        # return response
+        
+    except Exception as e:
+        return None
+
+
+try:
+    while True:
+        try:
+            ionscience = connect_ionscience(sys.argv[1])
+            print(ionscience)
+        except Exception as e2:
+            print(e2)
+            print("Reconnect PM");
+            sql = "UPDATE aqm_sensor_values SET AIN" + sys.argv[1] + " = '0' WHERE id = 1"
+            mycursor.execute(sql)
+            mydb.commit()
+
+        time.sleep(1)
+
+except Exception as e:
+    print(e)
