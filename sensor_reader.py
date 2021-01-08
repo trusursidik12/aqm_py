180bb6e86657f31d861dabe160446168da4a85e9
warih suryono <warih_suryono@yahoo.com>
2021-01-08 14:38:27 +0700

    Aaa

--- sensor_reader.py
+++ sensor_reader.py
@@ -322,7 +322,10 @@ try:
                 command = "ws_fws20n_reader.py"
             else:
                 command = "echo admin | sudo -S python3.5 ~/aqm_py/ws_fws20n_reader.py"
-
+        elif(rec[0] == "misol_sdr"):
+            subprocess.Popen("rtl_433 > ../misol_sdr.txt", shell=True)
+            time.sleep(3)
+            command = "echo admin | sudo -S python3.5 ~/aqm_py/ws_misol_sdr_reader.py"
         else:
             if sys.platform.startswith('win'):
                 command = "ws_davis_reader.py"
